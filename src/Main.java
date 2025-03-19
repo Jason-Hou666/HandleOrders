dc7c391978a049dd7d46673d3d4591f8166f2236
Jason-Hou666 <781215041@qq.com>
2025-03-19 15:38:03 +0800

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
-public class Main{
+public class Main {
     public static void main(String[] args) throws Exception {
         HandleOrders handleOrders = new HandleOrders();
+        OrderLogs orderLogs = new OrderLogs();
+        PizzaOrderQueue orderQueue = new PizzaOrderQueue();
+        
         handleOrders.takeOrder();
         handleOrders.createOrderSummary();
+        handleOrders.displayCustomPizzas();
         System.out.println(handleOrders);
+        
+        
+        orderLogs.handleLogs();
+        orderQueue.manageOrders();
     }
 }
